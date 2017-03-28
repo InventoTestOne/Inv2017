994b6f1e66919a20db2bad62cd361aa1b6860aaa
Arpit <desai.arpit@rocketmail.com>
2017-03-28 18:31:08 +0530

    Initial commit

---
+++ app/src/main/java/com/invento/somesh/invento2k17/NonTechnicalFragment.java
@@ -0,0 +1,41 @@
+package com.invento.somesh.invento2k17;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NonTechnicalFragment extends ListFragment {
+
+
+    String[] players={"Show Bizz(movie Making)","cut Throat(Solo Singing)","Feel THe BEat(Dance)",
+            "Rock the Range(Battle of Bands","vouge(The Fashion show"};
+
+
+
+    public NonTechnicalFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView=inflater.inflate(R.layout.fragment_non_technical, container,false);
+
+        ListAdapter adapter=new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,players);
+        setListAdapter(adapter);
+
+        return  rootView;
+    }
+
+}
---
+++ app/src/main/java/com/invento/somesh/invento2k17/TechnicalFragment.java
@@ -0,0 +1,62 @@
+package com.invento.somesh.invento2k17;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v4.widget.ListViewAutoScrollHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TechnicalFragment extends ListFragment {
+    String[] players={"Ander Herera","Wayne Rooney","David DeGea","Robin Van Persie",
+            "Juan Mata","Michael Carrick","Chris Smalling","Phil Jones"};
+
+
+    public TechnicalFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView=inflater.inflate(R.layout.fragment_technical, container,false);
+
+        ListAdapter adapter=new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,players);
+        setListAdapter(adapter);
+
+        return  rootView;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO Auto-generated method stub
+        Toast.makeText(getActivity(), getListView().getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+
+
+
+}
+
+
